dac8bc86dfa865063171674d33732f76ec4b385f
MarkosComK <markoscomks@gmail.com>
2023-07-24 14:24:44 +0100

    clean code changes in general

--- js/index.js
+++ js/index.js
@@ -1,112 +1,90 @@
+const menuLinks = document.querySelectorAll('.menu-links a');
+const scrollNav = document.getElementById('scroll-nav');
+const navLinks = document.querySelectorAll('.nav-links a');
+const themeToggle = document.getElementById('theme-toggle');
+const textElement = document.querySelector('.section__text__p2');
+const typingAnimationElement = document.getElementById('typing-animation');
+
 // Hamburguer Menu
 function toggleMenu() {
-  const menu = document.querySelector('.menu-links')
-  const icon = document.querySelector('.hamburguer-icon')
-
-  menu.classList.toggle('open')
-  icon.classList.toggle('open')
+  const menu = document.querySelector('.menu-links');
+  const icon = document.querySelector('.hamburguer-icon');
+  menu.classList.toggle('open');
+  icon.classList.toggle('open');
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const menuLinks = document.querySelectorAll('.menu-links a')
-
-  menuLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault()
-      toggleMenu()
-      const target = document.querySelector(link.getAttribute('href'))
-      target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      })
-    })
-  })
-})
+menuLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    toggleMenu();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
 
 window.addEventListener('scroll', () => {
-  const scrollNav = document.getElementById('scroll-nav')
   if (window.scrollY > 0) {
-    scrollNav.classList.add('scroll-active')
+    scrollNav.classList.add('scroll-active');
   } else {
-    scrollNav.classList.remove('scroll-active')
+    scrollNav.classList.remove('scroll-active');
   }
-})
+});
 
-window.addEventListener('DOMContentLoaded', () => {
-  const menuLinks = document.querySelectorAll('.nav-links a')
-
-  menuLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault()
-      const target = document.querySelector(link.getAttribute('href'))
-      target.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      })
-    })
-  })
-})
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
 
 // Dark Mode
-
 function changeColorScheme(newColorScheme) {
-  localStorage.colorScheme = newColorScheme
-
-  document.body.dataset.theme = newColorScheme
-  return
+  localStorage.colorScheme = newColorScheme;
+  document.body.dataset.theme = newColorScheme;
 }
 
 function toggleColorScheme() {
-  const newColorScheme = localStorage.colorScheme === 'dark' ? 'light' : 'dark'
-  return changeColorScheme(newColorScheme)
+  const newColorScheme = localStorage.colorScheme === 'dark' ? 'light' : 'dark';
+  changeColorScheme(newColorScheme);
 }
 
-const themeToggle = document.getElementById('theme-toggle')
-
-if (themeToggle) {
-  themeToggle.addEventListener('click', e => {
-    toggleColorScheme()
-  })
-}
+themeToggle.addEventListener('click', toggleColorScheme);
 
 if (localStorage.colorScheme) {
-  document.body.dataset.theme = localStorage.colorScheme
-} else if (
-  window.matchMedia &&
-  window.matchMedia('(prefers-color-scheme: dark)').matches
-) {
-  changeColorScheme('dark')
+  document.body.dataset.theme = localStorage.colorScheme;
+} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  changeColorScheme('dark');
 }
 
-// Listen for changes to the users preferences
-window
-  .matchMedia('(prefers-color-scheme: dark)')
-  .addEventListener('change', e => {
-    const newColorScheme = e.matches ? 'dark' : 'light'
-    changeColorScheme(newColorScheme)
-  })
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+  const newColorScheme = e.matches ? 'dark' : 'light';
+  changeColorScheme(newColorScheme);
+});
 
 // Animando texto
-
-const textElement = document.querySelector('.section__text__p2')
-const typingAnimationElement = document.getElementById('typing-animation')
-
-const text = '_Software Developer'
-let index = 0
+const text = '_Software Developer';
+let index = 0;
 
 function startTypingAnimation() {
   if (index < text.length) {
-    typingAnimationElement.textContent += text.charAt(index)
-    index++
-    setTimeout(startTypingAnimation, 100) // Ajuste a velocidade da digitação aqui (em milissegundos)
+    typingAnimationElement.textContent += text.charAt(index);
+    index++;
+    setTimeout(startTypingAnimation, 100); // Ajuste a velocidade da digitação aqui (em milissegundos)
   } else {
     // Reiniciar a animação
     setTimeout(() => {
-      typingAnimationElement.textContent = ''
-      index = 0
-      startTypingAnimation()
-    }, 2000) // Tempo de espera antes de reiniciar a animação (em milissegundos)
+      typingAnimationElement.textContent = '';
+      index = 0;
+      startTypingAnimation();
+    }, 2000); // Tempo de espera antes de reiniciar a animação (em milissegundos)
   }
 }
 
-startTypingAnimation()
+startTypingAnimation();
